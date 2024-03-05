002cd71e91faa482fd5145b3079a845933ea7a70
Alisson Labres Gimenes <alisson.labres@gmail.com>
2024-03-05 14:26:13 -0300

    [FIX] - Adjust router mock testing e2e

--- tests/mock/router/create-schedule.router.ts
+++ tests/mock/router/create-schedule.router.ts
@@ -3,23 +3,24 @@ import { createBulletsSuccess, createScheduleSuccess } from "../asserts/create-s
 
 export const mockingBullets = async (response: string) => {
   await fixture.page.route('**/api/v1/bullets', route => {
-    const routeFulfill: { [key: string]: Promise<void> } = {
-      'success': route.fulfill(createBulletsSuccess),
-      'error': route.fulfill({ status: 500, json: { message: "Internal server error" } }),
-      'empty': route.fulfill({ status: 200, json: [] }),
+
+    const routeFulfill: { [key: string]: () => Promise<void> } = {
+      'success': () => route.fulfill(createBulletsSuccess),
+      'error': () => route.fulfill({ status: 500, json: { message: "Internal server error" } }),
+      'empty': () => route.fulfill({ status: 200, json: [] }),
     }
 
-    routeFulfill[response];
+    routeFulfill[response]();
   });
 }
 
 export const mockingSchedule = async (response: string) => {
   await fixture.page.route('**/api/v1/schedule/appointment', route => {
-    const routeFulfill: { [key: string]: Promise<void> } = {
-      'success': route.fulfill(createScheduleSuccess),
-      'error': route.fulfill({ status: 500, json: { message: "Internal server error" } }),
+    const routeFulfill: { [key: string]: () => Promise<void> } = {
+      'success': () => route.fulfill(createScheduleSuccess),
+      'error': () => route.fulfill({ status: 500, json: { message: "Internal server error" } }),
     }
 
-    routeFulfill[response];
+    routeFulfill[response]();
   });
 }
--- tests/mock/router/list-users.router.ts
+++ tests/mock/router/list-users.router.ts
@@ -3,12 +3,12 @@ import { listUsersError, listUsersSuccess } from "../asserts/list-users.assert";
 
 export const mockingListUsers = async (response: string) => {
   await fixture.page.route('**/api/v1/users', route => {
-    const routeFulfill: { [key: string]: Promise<void> } = {
-      'success': route.fulfill(listUsersSuccess),
-      'error': route.fulfill(listUsersError),
-      'empty': route.fulfill({ status: 200, json: [] }),
+    const routeFulfill: { [key: string]: () => Promise<void> } = {
+      'success': () => route.fulfill(listUsersSuccess),
+      'error': () => route.fulfill(listUsersError),
+      'empty': () => route.fulfill({ status: 200, json: [] }),
     }
 
-    routeFulfill[response];
+    routeFulfill[response]();
   });
 }
